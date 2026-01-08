0071845251c04d6ef931b84806c39cde3e98b359
taehui <hth4236727@gmail.com>
2026-01-09 00:20:26 +0900

    eidt 수정기능 추가

--- emotion-diary-app/src/components/Editor.jsx
+++ emotion-diary-app/src/components/Editor.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Button from './Button'
 import './Editor.css'
 import EmotionItem from './EmotionItem'
@@ -44,14 +44,23 @@ const getStringedDate = (targetDate) => {
   return `${year}-${month}-${date}`
 }
 
-const Editor = ({ onSubmit }) => {
-  const nav = useNavigate()
-
+const Editor = ({ initData, onSubmit }) => {
   const [input, setInput] = useState({
     createdDate: new Date(),
     emotionId: 3,
     content: '',
   })
+
+  const nav = useNavigate()
+
+  useEffect(() => {
+    if (initData) {
+      setInput({
+        ...initData,
+        createdDate: new Date(Number(initData.createdDate)),
+      })
+    }
+  }, [initData])
 
   const onChangeInput = (e) => {
     let name = e.target.name
--- emotion-diary-app/src/pages/Edit.jsx
+++ emotion-diary-app/src/pages/Edit.jsx
@@ -2,13 +2,49 @@ import { useParams, useNavigate } from 'react-router-dom'
 import Header from '../components/Header'
 import Button from '../components/Button'
 import Editor from '../components/Editor'
+import { useContext, useEffect, useState } from 'react'
+import { DiaryDispatchContext, DiaryStateContext } from '../App'
 
 const Edit = () => {
   const params = useParams()
   const nav = useNavigate()
+  const { onDelete, onUpdate } = useContext(DiaryDispatchContext)
+  const data = useContext(DiaryStateContext)
+  const [curDiaryItem, setCurDiaryItem] = useState()
+
+  useEffect(() => {
+    const currentDiaryItem = data.find(
+      (item) => String(item.id) === String(params.id)
+    )
+    if (!currentDiaryItem) {
+      window.alert('존재하지 않는 일기입니다.')
+      // 홈페이지로 강제이동
+      nav('/', { replace: true })
+    }
+    setCurDiaryItem(currentDiaryItem)
+  }, [params.id])
 
   const onClickDelete = () => {
-    window.confirm('일기를 정말 삭제할까요? 다시 복구되지 않아요!')
+    if (window.confirm('일기를 정말 삭제할까요? 다시 복구되지 않아요!')) {
+      // 일기 삭제 로직
+      onDelete(params.id)
+      // 뒤로가기 방지
+      nav('/', { replace: true })
+    }
+  }
+
+  const onSubmit = (input) => {
+    if (window.confirm('일기를 정말 수정할까요?')) {
+      // 파라미터의 순서를 잘 지켜주기!!
+      onUpdate(
+        params.id,
+        input.createdDate.getTime(),
+        input.emotionId,
+        input.content
+      )
+    }
+    // 뒤로가기 방지
+    nav('/', { replace: true })
   }
 
   return (
@@ -20,7 +56,7 @@ const Edit = () => {
           <Button onClick={onClickDelete} text={'삭제하기'} type={'nagative'} />
         }
       />
-      <Editor />
+      <Editor initData={curDiaryItem} onSubmit={onSubmit} />
     </div>
   )
 }
